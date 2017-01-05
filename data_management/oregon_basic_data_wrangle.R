fe88bb604150e689dd8f4ade5a9476198ad7e115
Ryan Gan <ryangan@ryans-mbp.cvmbs.colostate.edu>
2017-01-05 15:58:44 -0700

    Updated code to import chunks of code, search for specific rows with specific icd9 codes, and output those rows in a new dataframe.

--- data_management/oregon_basic_data_wrangle.R
+++ data_management/oregon_basic_data_wrangle.R
@@ -6,7 +6,8 @@
 # ------------------------------------------------------------------------------
 
 # Note: Datasets are pretty large. I had to come up with some different ways of
-# processing these data.
+# processing these data. I checked the number of rows in the command line and
+# found there were 77,069,321 rows. 
 
 # Helpful sources: 
 # Reading in specific sequences of a large data set
@@ -37,16 +38,28 @@ stop_time
 
 oregon_df[1:10, 64]
 
+# code chunk to output rows of a specific outcome ------------------------------
+# see if your lists work with the oregon_df
+# primary diagnosis
+test_df <- oregon_df %>% 
+  # select columns to search through
+  select(matches("dx")) %>% 
+  # apply the %in% function to evaluate list of icd9 codes to subset by
+  lapply("%in%", outcome_icd9_list[[2]]) %>% 
+  # reduce the funciton to 'or' across all logic values
+  Reduce(f = "|", .) %>% 
+  # identifies the rows where this statement is true
+  which %>% 
+  slice(.data = oregon_df)
+# this chunk of code works for subsetting the rows that meet specific icd9 codes
+
+head(test_df)
+
 # get column df names
 df_col_names <- colnames(oregon_df)
 
 
-# Notes on first import
-head(oregon_df)
-glimpse(oregon_df)
-xtabs(~dx1, oregon_df)
-getwd()
-object.size(oregon_df)
+# code chunk to sequentially read in smaller chunks of dataframes --------------
 
 # figure out a test script to read chunks of the file and move on
 # number of lines to read
@@ -79,6 +92,9 @@ stop_time <-  Sys.time() - start_time
 
 stop_time
 
+glimpse(test_df)
+# find locations of variables
+which(colnames(test_df) == 'dx13')
 
 # check to make sure dataset imported
 head(test_df$V1)
@@ -86,16 +102,53 @@ head(oregon_df$personkey)
 tail(oregon_df$personkey)
 tail(test_df$V1)
 
+# combo -----
+# now try to work the two chunks together to identify records that contain icd9s
+# of a certain outcome and then output those rows in chunks of data.
 
 
-# code chunk to read the number of lines in the oregon data
-# zip path
-zip_path <- paste0("./data/gan_episodes_of_care.zip")
-testconnection <- file(zip_path, open="r")
-readsize <- 20000
-n_lines <- 0
-(while((linesread <- length(readLines(testconnection, readsize))) > 0) 
-  n_lines <- n_lines+linesread)
-close(testconnection)
-n_lines # 20,720,000, there are probably more records than this
+# I need a dataframe of start row and length to read after
+n_start<- seq(from = 1, to = 200000, by = 10000) 
+
+#n_start<- seq(from = 1, to = 100, by = 5)   
+# create dataframe
+n_read <- data_frame(n_start) %>% # turn the sequence in to a dataframe
+  mutate(n_length = 10000) # set length to read 
+
+# test dataframe
+start_time <- Sys.time()
+
+asthma_icd9_vector <- outcome_icd9_list[[2]]
+
+test_df2 <- do.call(rbind, apply(n_read,1, function(x) 
+  return(
+  fread("./data/oregon_epis_care_reduced.csv", sep =",",
+    nrows=x[2], skip=x[1], header = F, 
+    # keep all the variables as character to avoid import errors
+    # 72 variables
+    colClasses = rep("character", 72)) %>% 
+    # now search for icd9 codes
+    filter(V30 %in% asthma_icd9_vector |
+           V32 %in% asthma_icd9_vector |
+           V33 %in% asthma_icd9_vector |
+           V34 %in% asthma_icd9_vector |
+           V35 %in% asthma_icd9_vector |
+           V36 %in% asthma_icd9_vector |
+           V37 %in% asthma_icd9_vector |
+           V38 %in% asthma_icd9_vector |
+           V39 %in% asthma_icd9_vector |
+           V40 %in% asthma_icd9_vector |
+           V41 %in% asthma_icd9_vector |
+           V42 %in% asthma_icd9_vector | 
+           V43 %in% asthma_icd9_vector)  
+  ) # end of apply
+  ) # end of rbind
+  ) # end of do.call
+
+colnames(test_df2) <- df_col_names
+
+stop_time <-  Sys.time() - start_time 
+
+stop_time
+
 
