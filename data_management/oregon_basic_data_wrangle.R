e4840d6e394264fa201d5aa7fe2e853224e7b937
Ryan Gan <ryangan@Ryans-MacBook-Pro.local>
2017-01-06 15:56:16 -0700

    Got loop to work.

--- data_management/oregon_basic_data_wrangle.R
+++ data_management/oregon_basic_data_wrangle.R
@@ -9,24 +9,21 @@
 # processing these data. I checked the number of rows in the command line and
 # found there were 77,069,321 rows. 
 
-# Helpful sources: 
-# Reading in specific sequences of a large data set
-# http://stackoverflow.com/questions/21798930/how-to-read-specific-rows-of-csv-file-with-fread-function
-
 # load libraries ---------------------------------------------------------------
-library(tidyverse)
+library(dplyr)
 library(data.table)
-
 
 # import data ------------------------------------------------------------------
 
-# Read in first 100000 lines to mess around with in R until I figure out another
-# way. I will then delete the text file 
+# Read in first 100000 lines for exploration.
+
 #read_path <- paste0("./data/gan_episodes_of_care.txt")
-
 #oregon_df <- fread(read_path, sep = "|", nrows = 200000, showProgress = T)
-
 #fwrite(oregon_df, "./data/oregon_epis_care_reduced.csv")
+
+# I wrote a reduced dataframe of 200,000 rows to get an idea of data structure
+
+# import the reduced dataframe
 start_time <- Sys.time()
 
 oregon_df <- fread("./data/oregon_epis_care_reduced.csv", sep = ",", 
@@ -36,77 +33,19 @@ stop_time <-  Sys.time() - start_time
 # time it took
 stop_time
 
-oregon_df[1:10, 64]
+#oregon_df[1:10, 64]
 
-# code chunk to output rows of a specific outcome ------------------------------
-# see if your lists work with the oregon_df
-# primary diagnosis
-test_df <- oregon_df %>% 
-  # select columns to search through
-  select(matches("dx")) %>% 
-  # apply the %in% function to evaluate list of icd9 codes to subset by
-  lapply("%in%", outcome_icd9_list[[2]]) %>% 
-  # reduce the funciton to 'or' across all logic values
-  Reduce(f = "|", .) %>% 
-  # identifies the rows where this statement is true
-  which %>% 
-  slice(.data = oregon_df)
-# this chunk of code works for subsetting the rows that meet specific icd9 codes
+# Define read paths ------------ -----------------------------------------------
+# import r list of outcome-specific icd9 codes
+load_path <- paste0("./data/outcome_list.RData")
+load(load_path)
 
-head(test_df)
+# path and file name of data
+read_path <- paste0("./data/oregon_epis_care_reduced.csv")
 
-# get column df names
-df_col_names <- colnames(oregon_df)
-
-
-# code chunk to sequentially read in smaller chunks of dataframes --------------
-
-# figure out a test script to read chunks of the file and move on
-# number of lines to read
-
-# I need a dataframe of start row and length to read after
-n_start<- seq(from = 1, to = 200000, by = 10000) 
-
-#n_start<- seq(from = 1, to = 100, by = 5)   
-# create dataframe
-n_read <- data_frame(n_start) %>% # turn the sequence in to a dataframe
-  mutate(n_length = 10000) # set length to read 
-
-# test dataframe
-start_time <- Sys.time()
-
-test_df <- do.call(rbind, apply(n_read,1, function(x) 
-  return(fread("./data/oregon_epis_care_reduced.csv", sep =",",
-               nrows=x[2], skip=x[1], header = F, 
-               # keep all the variables as character to avoid import errors
-               # 72 variables
-               colClasses = rep("character", 72))) %>% 
-    # allows me to subset (1st of Jan in this case)
-    # i want to filter by scanning through each dx column and looking 
-    # for specific outcomes
-    filter(V24 == "2013-11-01")))
-
-colnames(test_df) <- df_col_names
-
-stop_time <-  Sys.time() - start_time 
-
-stop_time
-
-glimpse(test_df)
-# find locations of variables
-which(colnames(test_df) == 'dx13')
-
-# check to make sure dataset imported
-head(test_df$V1)
-head(oregon_df$personkey)
-tail(oregon_df$personkey)
-tail(test_df$V1)
-
-# combo -----
 # now try to work the two chunks together to identify records that contain icd9s
 # of a certain outcome and then output those rows in chunks of data.
 
-
 # I need a dataframe of start row and length to read after
 n_start<- seq(from = 1, to = 200000, by = 10000) 
 
@@ -115,40 +54,59 @@ n_start<- seq(from = 1, to = 200000, by = 10000)
 n_read <- data_frame(n_start) %>% # turn the sequence in to a dataframe
   mutate(n_length = 10000) # set length to read 
 
-# test dataframe
-start_time <- Sys.time()
+# read first line and get column names
+df_col_names <- fread(read_path, nrows = 0, header = T, 
+                      colClasses = rep("character", 72))
 
-asthma_icd9_vector <- outcome_icd9_list[[2]]
+col_names <- colnames(df_col_names) # output column names
 
-test_df2 <- do.call(rbind, apply(n_read,1, function(x) 
+# loop to output dataframes of outcomes ----------------------------------------
+start_time <- Sys.time() # start clock time
+
+for(i in 1:length(outcome_icd9_list)) { # start loop
+
+outcome_name <- names(outcome_icd9_list)[i] # output the name of the list/outcome
+icd9_vector <- outcome_icd9_list[[i]] # output the icd9 codes of the list
+
+outcome_df <- do.call(rbind, apply(n_read,1, function(x) 
   return(
-  fread("./data/oregon_epis_care_reduced.csv", sep =",",
+  fread(read_path, sep =",",
     nrows=x[2], skip=x[1], header = F, 
     # keep all the variables as character to avoid import errors
     # 72 variables
     colClasses = rep("character", 72)) %>% 
     # now search for icd9 codes
-    filter(V30 %in% asthma_icd9_vector |
-           V32 %in% asthma_icd9_vector |
-           V33 %in% asthma_icd9_vector |
-           V34 %in% asthma_icd9_vector |
-           V35 %in% asthma_icd9_vector |
-           V36 %in% asthma_icd9_vector |
-           V37 %in% asthma_icd9_vector |
-           V38 %in% asthma_icd9_vector |
-           V39 %in% asthma_icd9_vector |
-           V40 %in% asthma_icd9_vector |
-           V41 %in% asthma_icd9_vector |
-           V42 %in% asthma_icd9_vector | 
-           V43 %in% asthma_icd9_vector)  
+    filter(V30 %in% icd9_vector |
+           V32 %in% icd9_vector |
+           V33 %in% icd9_vector |
+           V34 %in% icd9_vector |
+           V35 %in% icd9_vector |
+           V36 %in% icd9_vector |
+           V37 %in% icd9_vector |
+           V38 %in% icd9_vector |
+           V39 %in% icd9_vector |
+           V40 %in% icd9_vector |
+           V41 %in% icd9_vector |
+           V42 %in% icd9_vector | 
+           V43 %in% icd9_vector)  
   ) # end of apply
   ) # end of rbind
   ) # end of do.call
 
-colnames(test_df2) <- df_col_names
+colnames(outcome_df) <- col_names # assign column names to dataframe
 
+# write csv file and assign outcome name to df name
+save_name <- paste0("./data/oregon_",outcome_name, "_claims_2013.csv")
+fwrite(outcome_df, save_name)
+
+} # end of loop
+# stop time of clock
 stop_time <-  Sys.time() - start_time 
-
 stop_time
 
+# estimated time to run through entire file
+#((33.5/200000)*27000000)/60
+# 75 minutes
 
+hf_df <- fread("./data/oregon_heart_failure_claims_2013.csv", sep = ",")
+#head(hf_df)
