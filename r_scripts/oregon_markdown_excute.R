1c3a3b5db836b9f7b87903d3568f732c000cf6ac
AprilJing <ljy199341@163.com>
2017-03-02 14:48:58 -0700

    Script for excuting R markdown file

---
+++ r_scripts/oregon_markdown_excute.R
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------------
+# Title: Script to excute descriptive summary R markdown file
+# Author: Jingyang Liu
+# Date: 3/2/2016
+# ------------------------------------------------------------------------------
+
+
+# call libraries
+library(rmarkdown)
+
+rmarkdown::render('./oregon_clean.rmd')
+
