bd2c15a7e373248fed81901c016364ae0ba84586
AprilJing <ljy199341@163.com>
2017-03-09 13:39:09 -0700

    Update oregon_casecrossover.R
    write full loop case crossover study over all diseases

--- r_scripts/oregon_casecrossover.R
+++ r_scripts/oregon_casecrossover.R
@@ -12,16 +12,6 @@ library(readxl)
 getwd()
 setwd("C:/Users/jyliu/Desktop/local_git_repo/oregon_wildfire/data_disease")
 
-
-read_path <- paste0('../data_new/or_hosp_w_outcome_df.csv')
-disease <- read_csv(read_path) 
-
-disease <- disease %>%
-  # add new transverted from date
-  mutate(dates = as.Date(fromdate, "%m/%d/%Y"))
-
-
-
 ### ---------------------------------------------------------------------
 var_list <- c('respiratory', 'asthma', 'pneumonia',  'acute_bronch', 'copd', 
               'cvd', 'isch_heart_dis', 'arrhythmia', 'heart_failure', 
@@ -42,10 +32,10 @@ for (i in var_list){
     full_join(or_disease, by = c("personkey", "clmid")) %>% 
     arrange(personkey, clmid, line, fromdate) %>% 
     filter(row_number() == 1) %>%
+    # add new transverted from date
     mutate(dates = as.Date(fromdate, "%m/%d/%Y")) %>%
     filter(dates >= '2013-07-01' & 
-           dates <= '2013-10-31') %>%
-    mutate(outcome = 1)
+           dates <= '2013-10-31') 
   
   # Create a permanent case-cross over dataset
   file_name <- paste('oregon', i, 'jul_to_oct_claim.csv', sep = '_')
@@ -59,43 +49,30 @@ total_time # Time difference of 5 mins
 
 setwd("../data_new/")
 
+
+
 ## Case crossover for each disease
-var_list2 <- c('resp1', 'asthma1', 'pneum1',  'acute_bronch1', 'copd1', 'cvd1', 
-              'ihd1', 'arrhythmia1', 'hf1', 'cereb_vas1', 'mi1', 'broken_arm1')
 
 start <- Sys.time()
-for(m in var_list2){ # begin first loop of variable names (outcomes)
-  
-  # Case-Crossover loop --------------------------------------------------------
-  outcome_col <- which(colnames(disease) == m) # use to keep outcome var
+for(m in var_list){ # begin first loop of variable names (outcomes)
+
+  read_path <- paste('oregon', m, 'jul_to_oct_claim.csv', sep='_')
+  disease <- read_csv( read_path)
   
   outcome_id <- disease %>%
-    # filter total outcome for jth disease
-    filter(disease[[m]] == 1) %>% 
-    # filter date in July 1, 2012 and Oct 31, 2012
-    filter(dates >= '2013-07-01' & 
-             dates <= '2013-10-31') %>%
     # arrange with dates
     arrange(dates) %>%
-    mutate(id = seq(1, nrow(.), by = 1)) %>% # create subject id
-    # select cols, the 4th is disease name
-    select(personkey, id, fromdate, dates, 
-           (outcome_col), # keep in bracket for outcome var num
-           dx1, ZIP, race, ethn, MSA, pos, sex_ind, age_ind)
-  
-  outcome_col2 <- which(colnames(outcome_id) == m) # use to keep outcome var
-  
+    mutate(id = seq(1, nrow(.), by = 1))  # create subject id
+   
   # create dataset to populate
-  id_date_df <- data_frame(personkey = NA, id =NA, fromdate = NA, dates = NA, 
-                           dx1 = NA, ZIP = NA, race = NA, ethn = NA, 
-                           MSA = NA, pos = NA, sex_ind = NA, age_ind = NA)
+  id_date_df <- data_frame()
   
   
   # begin second loop to create counterfactual observations for each case subject
   for (k in 1:nrow(outcome_id)){
     
     # find the replicate times of weeks
-    dates_l <- outcome_id[[k,4]] 
+    dates_l <- outcome_id[[k,74]] 
     n1 <- 0
     d=as.Date("2013-07-01")
     i=1
@@ -108,7 +85,7 @@ for(m in var_list2){ # begin first loop of variable names (outcomes)
     d[1:n1-1] # shows character(0) when the first week
     n1-1
     
-    dates_l <- outcome_id[[k,4]] 
+    dates_l <- outcome_id[[k,74]] 
     n2=0
     e=as.Date("2013-10-31")
     j=1
@@ -125,19 +102,19 @@ for(m in var_list2){ # begin first loop of variable names (outcomes)
     # and make conuterfactual dates
     if (n1==1){
       cov_df <- do.call("bind_rows", replicate(n1+n2-1, outcome_id[k,],simplify = F))
-      cov_df$dates <- c(outcome_id[[k,4]], e[1:n2-1])
+      cov_df$dates <- c(outcome_id[[k,74]], e[1:n2-1])
     } else if (n2==1){
       cov_df <- do.call("bind_rows", replicate(n1+n2-1, outcome_id[k,],simplify = F))
-      cov_df$dates <- c(outcome_id[[k,4]], d[1:(n1-1)])
+      cov_df$dates <- c(outcome_id[[k,74]], d[1:(n1-1)])
     }else{
       cov_df <- do.call("bind_rows", replicate(n1+n2-1, outcome_id[k,],simplify = F))
-      cov_df$dates <- c(outcome_id[[k,4]], d[1:(n1-1)], e[1:n2-1])
+      cov_df$dates <- c(outcome_id[[k,74]], d[1:(n1-1)], e[1:n2-1])
     }
     
     # bind unique id and date of the year with covariates
     id_date <- bind_cols(cov_df)
     # iteration which binds rows of unique ids
-    id_date_df <- na.omit(bind_rows(id_date_df, id_date))
+    id_date_df <- bind_rows(id_date_df, id_date)
     
   } # End of inner loop
   
@@ -147,20 +124,37 @@ for(m in var_list2){ # begin first loop of variable names (outcomes)
     arrange(id, dates) # order by id and date
   
   # Create a permanent case-cross over dataset
-  file_name <- paste(m, 'jul_to_oct_casecross.csv', sep = '_')
+  file_name <- paste('or', m, 'jul_to_oct_casecross.csv', sep = '_')
   
   # write permanent dataset
-  write_csv(outcome_casecross, paste0("../data_new/", file_name))
+  write_csv(outcome_casecross, paste0("./", file_name))
 } # End of the overall loop
 
 # sweet this works
 total_time <- Sys.time() - start
-total_time # Time difference of 48.35907 mins
+total_time # Time difference of 1.305516 hours
+
+
+## Check
+read_path <- paste0("or_asthma_jul_to_oct_casecross.csv")
+sam_1 <- read_csv(read_path)
+
+head(sam_1, 20L)
+
+check <- sam_1 %>% filter(outcome == 1) %>% 
+  group_by(personkey) %>% 
+  summarise(n = n())
+
+summary(check)
+
+check2 <- sam_1 %>% filter(personkey=="409060") %>% 
+  select(personkey, fromdate, todate, dates, outcome, dx1, ZIP)
+
+read_path2 <- paste0()
 
 
 
-
-
+## Try -------------------------------------------------------------------------
 # Asthma1 ----------------------------------------------------------------------
 read_path_ast <- paste0('oregon_asthma_jul_to_oct_claim.csv')
 asthma_df <- read_csv(read_path_ast)
@@ -174,26 +168,26 @@ one_asthma1_case <- sample_n(disease_asthma1, 1)
 one_asthma1_case
 
 # find the replicate times of weeks
-dates <- one_asthma1_case[[1,74]] 
+sam_date <- one_asthma1_case[[1,74]] 
 n1 <- 0
 d=as.Date("2013-07-01")
 i=1
-while (dates >= "2013-07-01"){
-  dates <- dates - 7
-  d[i] = dates
+while (sam_date >= "2013-07-01"){
+  sam_date <- sam_date - 7
+  d[i] = sam_date
   i = i+1
   n1 = n1+1
 }
 d[1:n1-1] # shows character(0) when the first week
 n1-1
 
-dates <- one_asthma1_case[[1,74]] 
+sam_date <- one_asthma1_case[[1,74]] 
 n2=0
 e=as.Date("2013-10-31")
 j=1
-while (dates <= "2013-10-31"){
-  dates <- dates + 7
-  e[j]=dates
+while (sam_date <= "2013-10-31"){
+  sam_date <- sam_date + 7
+  e[j]=sam_date
   j=j+1
   n2 = n2 + 1
 }
@@ -204,133 +198,17 @@ n2-1
 # and make conuterfactual dates
 if (n1==1){
   cov_df <- do.call("bind_rows", replicate(n1+n2, one_asthma1_case,simplify = F))
-  cov_df$dates <- c(one_asthma1_case[[1,3]], e[1:n2-1])
+  cov_df$dates <- c(one_asthma1_case[[1,74]], e[1:n2-1])
 } else if (n2==1){
   cov_df <- do.call("bind_rows", replicate(n1+n2, one_asthma1_case,simplify = F))
-  cov_df$dates <- c(one_asthma1_case[[1,3]], d[1:(n1-1)])
+  cov_df$dates <- c(one_asthma1_case[[1,74]], d[1:(n1-1)])
 }else{
   cov_df <- do.call("bind_rows", replicate(n1+n2-1, one_asthma1_case,simplify = F))
-  cov_df$dates <- c(one_asthma1_case[[1,3]], d[1:(n1-1)], e[1:n2-1])
+  cov_df$dates <- c(one_asthma1_case[[1,74]], d[1:(n1-1)], e[1:n2-1])
 }
 
 # make outcome variable, 1 means asthma1 and 0 means no asthma1
 cov_df <- cov_df %>%
-  mutate(outcome = ifelse(dates == one_asthma1_case[[1,3]], 1, 0)) %>%
+  mutate(outcome2 = ifelse(dates == one_asthma1_case[[1,74]], 1, 0)) %>%
   arrange(id, dates) # order by id and date
-
-
-
-### Every diseases--------------------------------------------------------------
-# Make 12 separate data frames (excel) for each disease for dx1
-# 01/31/2017-02/01/2017
-
-var_list <- c('resp1', 'asthma1', 'pneum1',  'acute_bronch1', 'copd1', 'cvd1', 
-              'ihd1', 'arrhythmia1', 'hf1', 'cereb_vas1', 'mi1', 'broken_arm1')
-
-start <- Sys.time()
-for(m in var_list){ # begin first loop of variable names (outcomes)
-  
-  # Case-Crossover loop --------------------------------------------------------
-  outcome_col <- which(colnames(disease) == m) # use to keep outcome var
-  
-  outcome_id <- disease %>%
-    # filter total outcome for jth disease
-    filter(disease[[m]] == 1) %>% 
-    # filter date in July 1, 2012 and Oct 31, 2012
-    filter(dates >= '2013-07-01' & 
-             dates <= '2013-10-31') %>%
-    # arrange with dates
-    arrange(dates) %>%
-    mutate(id = seq(1, nrow(.), by = 1)) %>% # create subject id
-    # select cols, the 4th is disease name
-    select(personkey, id, fromdate, dates, 
-           (outcome_col), # keep in bracket for outcome var num
-           dx1, ZIP, race, ethn, MSA, pos, sex_ind, age_ind)
-  
-  outcome_col2 <- which(colnames(outcome_id) == m) # use to keep outcome var
-  
-  # create dataset to populate
-  id_date_df <- data_frame(personkey = NA, id =NA, fromdate = NA, dates = NA, 
-                           dx1 = NA, ZIP = NA, race = NA, ethn = NA, 
-                           MSA = NA, pos = NA, sex_ind = NA, age_ind = NA)
-  
-  
-  # begin second loop to create counterfactual observations for each case subject
-  for (k in 1:nrow(outcome_id)){
-    
-    # find the replicate times of weeks
-    dates_l <- outcome_id[[k,4]] 
-    n1 <- 0
-    d=as.Date("2013-07-01")
-    i=1
-    while (dates_l >= "2013-07-01"){
-      dates_l <- dates_l - 7
-      d[i] = dates_l
-      i = i+1
-      n1 = n1+1
-    }
-    d[1:n1-1] # shows character(0) when the first week
-    n1-1
-    
-    dates_l <- outcome_id[[k,4]] 
-    n2=0
-    e=as.Date("2013-10-31")
-    j=1
-    while (dates_l <= "2013-10-31"){
-      dates_l <- dates_l + 7
-      e[j]=dates_l
-      j=j+1
-      n2 = n2 + 1
-    }
-    e[1:n2-1] # shows character(0) when the last week
-    n2-1
-    
-    # replicate covariates length of counterfactual dates
-    # and make conuterfactual dates
-    if (n1==1){
-      cov_df <- do.call("bind_rows", replicate(n1+n2-1, outcome_id[k,],simplify = F))
-      cov_df$dates <- c(outcome_id[[k,4]], e[1:n2-1])
-    } else if (n2==1){
-      cov_df <- do.call("bind_rows", replicate(n1+n2-1, outcome_id[k,],simplify = F))
-      cov_df$dates <- c(outcome_id[[k,4]], d[1:(n1-1)])
-    }else{
-      cov_df <- do.call("bind_rows", replicate(n1+n2-1, outcome_id[k,],simplify = F))
-      cov_df$dates <- c(outcome_id[[k,4]], d[1:(n1-1)], e[1:n2-1])
-    }
-    
-    # bind unique id and date of the year with covariates
-    id_date <- bind_cols(cov_df)
-    # iteration which binds rows of unique ids
-    id_date_df <- na.omit(bind_rows(id_date_df, id_date))
-    
-  } # End of inner loop
-  
-  # make outcome variable, 1 means asthma1 and 0 means no asthma1
-  outcome_casecross <- id_date_df %>%
-    mutate(outcome = ifelse(dates == as.Date(fromdate, "%m/%d/%Y"), 1, 0)) %>%
-    arrange(id, dates) # order by id and date
-  
-  # Create a permanent case-cross over dataset
-  file_name <- paste(m, 'jul_to_oct_casecross.csv', sep = '_')
-  
-  # write permanent dataset
-  write_csv(outcome_casecross, paste0("../data_new/", file_name))
-} # End of the overall loop
-
-# sweet this works
-total_time <- Sys.time() - start
-total_time # Time difference of 48.35907 mins
-
-
-getwd()
-# check
-aaa <- disease %>% filter(dates >= '2013-07-01' & 
-                            dates <= '2013-10-31')
-which(colnames(aaa)=='broken_arm1') # 153
-check_broken <- sum(aaa[[153]] == 1) # 74
-file_path <- paste0("../data_new/broken_arm1_jul_to_oct_casecross.csv")
-
-# read in dataframe 
-check <- read_csv(file_path)
-
 
