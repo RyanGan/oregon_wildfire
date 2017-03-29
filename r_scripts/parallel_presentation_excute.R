55995ccae7b52de07731c9391b214c269e9a4b62
AprilJing <ljy199341@163.com>
2017-03-29 10:40:17 -0600

    Add files via upload
    Run rmarkdown

---
+++ r_scripts/parallel_presentation_excute.R
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------------
+# Title: Script to excute descriptive summary R markdown file
+# Author: Jingyang Liu
+# Date: Mar 29, 2017
+# ------------------------------------------------------------------------------
+
+
+# call libraries
+library(rmarkdown)
+
+# call markdown doc.
+rmarkdown::render('./parallel_presentation.Rmd')
+
+
