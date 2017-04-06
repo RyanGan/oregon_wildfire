e0fc41923cb2379a60f2f4169ff3beba790b9a25
AprilJing <ljy199341@163.com>
2017-04-06 17:01:01 -0600

    lag

--- r_scripts/or_2013_smoke_jul_oct_time_stratified_case_crossover_server.R
+++ r_scripts/or_2013_smoke_jul_oct_time_stratified_case_crossover_server.R
@@ -102,226 +102,75 @@ zip_smoke_w_lag <- zip_smoke %>% arrange(ZIPCODE, date) %>%
 #          geo_smk_pm_lag2_zip, geo_smk_pm_lag3_zip) # looks good
 
 
-# Infile the Permanent Cleaned CHARS 2012 DataFrame ----------------------------
-# Put this file on the atmos server ASAP
-read_chars <- paste0('../../data/health_data/chars_2012_confidential.csv')
-# read in chars cleaned data
-chars_2012_conf_df <- read_csv(read_chars)
-# check data
-glimpse(chars_2012_conf_df)
 
-# find how many admissions overall were during the july 1 to oct31 timeframe
-#n_period <- chars_2012_conf_df %>% 
-#  filter(ADM_DATE >= "2012-07-01" & ADM_DATE <= "2012-10-31") %>% 
-#  nrow()
-
-#n_period
-
-
-# look up admit type, may want to subset to specifc admit
-# ADM_TYPE variable: 1 = emergency, 2 = urgent, 3 = elective, 4 = newborn
-# 5 = trauma, 9 = info not available
-summary(as.factor(chars_2012_conf_df$ADM_TYPE))
-# when I run analyses, I may want to limit only to emergency or urgent
-
-# Adding in county names based on the COUNTYRES variable in CHARS data
-# COUNTYRES values can be found in the CHARS data key
-# Join CHARS data with names of Washington counties 
-
-## NOTE 10/3/16: I should actually join these county names in the code cleaning
-# and creating variables for the CHARS dataset
-
-# Create two vectors for CHARS county code to bind in with CHARS data
-COUNTYRES <- c("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
-               "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21",
-               "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32",
-               "33", "34", "35", "36", "37", "38", "39", "99")
-
-county <- c("not_wa_residence", "Adams", "Asotin", "Benton", "Chelan", 
-            "Clallam", "Clark", "Columbia", "Cowlitz", "Douglas", "Ferry", "Franklin",
-            "Garfield", "Grant", "Grays Harbor", "Island", "Jefferson", "King",
-            "Kitsap", "Kittitas", "Klickitat", "Lewis", "Lincoln", "Mason", "Okanogan", 
-            "Pacific", "Pend Oreille", "Pierce", "San Juan", "Skagit", "Skamania",
-            "Snohomish", "Spokane", "Stevens", "Thurston", "Wahkiakum", "Walla Walla",
-            "Whatcom", "Whitman", "Yakima", "Unknown")
-
-chars_county <- data.frame(cbind(COUNTYRES, county))
-# convert the variables to characters to make merges easier
-chars_county$COUNTYRES <- as.character(chars_county$COUNTYRES)
-chars_county$county <- as.character(chars_county$county)
-
-# join county name in to CHARS data
-# create a working dataset 'chars_2012'
-chars_2012_to_loop <- chars_2012_conf_df %>% 
-  # filter to 2012 dates (prevents parsing failures later on)
-  filter(admit_date_impute >= "2012-01-01" & admit_date_impute <= "2012-12-31") %>% 
-  # join in the washington county name based on CHARS county identifier
-  full_join(chars_county, by = "COUNTYRES")
-
-summary(chars_2012_to_loop)
-
-
-# Setup for parallel computing before for loop ---------------------------------
-cores <- detectCores()
-cl <- makeCluster(cores) # use half the cores on the vet cluster
-registerDoParallel(cl)
-# load dplyr and lubridate packages on each cluster
-clusterCall(cl, function() library(dplyr))
-clusterCall(cl, function() library(lubridate))
-clusterCall(cl, function() library(readr))
-# since I have another foreach loop, I need to load foreach on the clusters
-clusterCall(cl, function() library(foreach))
-clusterCall(cl, function() library(doParallel))
-
-# Case-crossover datasets ------------------------------------------------------
-
+setwd("../../data_new/update/")
 
 # set variable list
-var_list <- c('resp1', 'asthma1', 'copd_ex1', 'copd1', 'pneum1', 'acute_bronch1',
-              'cvd1', 'mi1', 'arrhythmia1', 'cereb_vas1', 'ihd1', 'hf1', 'ra1',
-              'broken_arm1')
-# remove non-trauma, resp1 and pneum (way too big for this loop)
-# var_list <- c('arrhythmia1', 'cereb_vas1', 'ihd1', 'hf1')
-# still to large to efficiently make datasets on this keyboard. Need to refine.
+var_list <- c('respiratory', 'asthma', 'pneumonia',  'acute_bronch', 'copd', 
+              'cvd', 'isch_heart_dis', 'arrhythmia', 'heart_failure', 
+              'cerbrovas_dis', 'myocardial_infarc', 'broken_arm')
 
 start <- Sys.time()
-# parallele computing line
-foreach(j=1:length(var_list)) %dopar% { # begin first loop of variable names (outcomes)
+for(j in var_list) { # begin first loop of variable names (outcomes)
   
   # standard for statement without parallel computing
   #for(j in 1:length(var_list)){
+  read_path <- paste('or', j, 'may_to_sep_casecross.csv', sep='_')
+  or_disease <- read_csv(read_path)
   
-  # Case-Crossover loop ----------------------------------------------------------
-  outcome_name <- var_list[j]
+  ### try join
+  colnames(or_disease)[24] <- c("ZIPCODE")
+  colnames(or_disease)[74] <- c("date")
   
-  outcome_col <- which(colnames(chars_2012_to_loop) == outcome_name)
-  
-  outcome_id <- chars_2012_to_loop %>% 
-    filter(chars_2012_to_loop[[var_list[j]]] == 1) %>%  # jth outcome
-    filter(admit_date_impute >= '2012-07-01' & 
-             admit_date_impute <= '2012-10-31') %>% 
-    filter(STATERES == 'WA') %>% # limit to washington 
-    filter(ADM_TYPE == 1 | ADM_TYPE == 2) %>% # limit to ER/Urgent care visits
-    arrange(admit_date_impute) %>%
-    # limit to 1 visit per subject
-    select(PATIENTID, (outcome_col), # keep in bracket for outcome var num
-           admit_date_impute, DIS_DATE, ADM_TYPE, LENSTAYD, ZIPCODE, 
-           COUNTYRES, county, AGE, SEX, sex_num, race_nhw, age_cat) %>%
-    mutate(date_admit = admit_date_impute,
-           date_discharge = DIS_DATE,
-           length_stay = LENSTAYD) %>%
-    arrange(PATIENTID, admit_date_impute) %>% 
-    # generate a enumerated variable to indicate number of obs by patid
-    group_by(PATIENTID) %>% 
-    mutate(obs_num = seq_along(PATIENTID)) %>% 
-    select(-admit_date_impute, -DIS_DATE, -LENSTAYD)
-  
-  # I want to remove people with multiple visits
-  # find people with multiple visits and make an indicator
-  outcome_count <- outcome_id %>% group_by(PATIENTID) %>% 
-    summarise(obs = n()) %>% mutate(multi_obs = ifelse(obs > 1, 1, 0))
-  
-  # merge an indicator of multi obs back in to outcome_id and check the patients
-  # with multiple visits and limit to patients who only come in once
-  single_visits <- outcome_id %>% left_join(outcome_count, by = 'PATIENTID') %>% 
-    filter(multi_obs == 0)
-  
-  # for asthma: 1765 - 1638
-  # lost 127 observations with multiple visits
-  
-  outcome_col2 <- which(colnames(single_visits) == outcome_name) # use to keep outcome var
-  
-  # create dataset to populate
-  id_date_df <- data_frame()
-  
-  # I likely need to create a second cluster
-  #cores2 <- detectCores()
-  #cl2 <- makeCluster(cores2) # use half the cores on the vet cluster
-  #registerDoParallel(cl2)
-  # load dplyr and lubridate packages on each cluster
-  #clusterCall(cl2, function() library(dplyr))
-  #clusterCall(cl2, function() library(lubridate))
-  #clusterCall(cl2, function() library(readr))
-  # since I have another foreach loop, I need to load foreach on the clusters
-  #clusterCall(cl2, function() library(foreach))
-  #clusterCall(cl2, function() library(doParallel))
-  
-  # begin second loop to create counterfactual observations for each case subject
-  # parallele computing line 
-  #  foreach(i=1:nrow(single_visits)) %dopar% {
-  
-  #  standard line
-  for(i in 1:nrow(single_visits)){
-    # code dates for each id up to two months before and after the event (56 days)
-    # note 10/10/16, trying the entire referent periods of 126
-    date <- seq(as.Date(single_visits[[i, 12]] - 126), 
-                # consider every day
-                as.Date(single_visits[[i, 12]] + 126), by = '1 week') 
-    
-    # covariates to preserve
-    covariate <- single_visits[i, ] %>% 
-      select(PATIENTID, (outcome_col2), ZIPCODE, COUNTYRES, county, 
-             ADM_TYPE, date_admit, multi_obs, date_discharge, 
-             length_stay, AGE, age_cat, sex_num, race_nhw)
-    # replicate covariates length of counterfactual dates
-    cov_df <- do.call("bind_rows", replicate(length(date), covariate, 
-                                             simplify = F))
-    
-    # bind unique id and date of the year with covariates
-    id_date <- data_frame(date) %>% bind_cols(cov_df)
-    # iteration which binds rows of unique ids
-    id_date_df <- bind_rows(id_date_df, id_date)
-  } # end inner loop
-  
-  # stop second cluster
-  #stopCluster(cl2)
-  
-  # join with outcome
-  outcome_casecross <- id_date_df %>% 
-    mutate(date_admit = as.Date(date_admit, '%Y-%m-%d')) %>%
-    mutate(outcome = ifelse(date_admit == date, 1, 0)) %>%
-    # filter to July 1st to Oct 31st
-    filter(date >= "2012-07-1" & date <= "2012-10-31") %>% 
-    # join with zip-level pm estimates 
-    left_join(zip_smoke_w_lag, by = c("date", "ZIPCODE")) %>%
-    # join with county-level pm estimates
-    left_join(county_smoke_w_lag, by = c("date", "county")) %>% 
+  outcome_casecross <- or_disease %>%
+    # indicator for male=0, female=1, unknown = 2
+    mutate(age = 2013-yob,
+           sex_ind =ifelse(gender == "F", 1, 
+                    ifelse(gender == "M", 0, 2)),
+           age_ind = ifelse(age < 15, 0,
+                     ifelse(age >= 15 & age < 65, 1,
+                     ifelse(age >= 65 & age <=105, 2, NA)))
+           ) %>% # end of mutate 
     # create variables
-    mutate(day = as.factor(weekdays(date)),
-           day_admit = as.factor(weekdays(date_admit)),
-           month_smk = month(date),
-           month_admit = month(date_admit),
-           los = as.numeric(date_discharge - date_admit), 
-           season_admit = ifelse(date_admit >= "2012-06-22" &  
-                                   date_admit <= "2012-09-22", "summer",
-                                 ifelse(date_admit >= "2012-09-23" & 
-                                          date_admit <= "2012-12-21", "fall", "other")),
-           season_smk = ifelse(date >= "2012-06-22" &  
-                                 date <= "2012-09-22", "summer",
-                               ifelse(date >= "2012-09-23" & 
-                                        date <= "2012-12-21", "fall", "other"))) %>%
-    arrange(PATIENTID, date) # order by id and date
+    mutate(day = as.factor(weekdays(fromdate)),
+           day_admit = as.factor(weekdays(date)),
+           month_smk = month(fromdate),
+           month_admit = month(date),
+           season_smk = ifelse(fromdate >= "2013-03-20" &  
+                               fromdate <= "2013-06-21", "spring",
+                        ifelse(fromdate >= "2013-06-22" &  
+                               fromdate <= "2013-09-22", "summer",
+                        ifelse(fromdate >= "2013-09-23" & 
+                              fromdate <= "2013-12-21", "fall", "other"))),
+           season_admit = ifelse(date >= "2013-03-20" &  
+                                 date <= "2013-06-21", "spring",
+                          ifelse(date >= "2013-06-22" &  
+                                 date <= "2013-09-22", "summer",
+                          ifelse(date >= "2013-09-23" & 
+                                 date <= "2013-12-21", "fall", "other")))) %>%
+      # join with zip-level pm estimates
+    left_join(zip_smoke_w_lag, by = c("date", "ZIPCODE"))%>%
+    arrange(personkey, fromdate) # order by id and fromdate
+  
+  
+   
   
   # checks
-  #glimpse(outcome_casecross)
-  #which(colnames(outcome_casecross)=='geo_wt_pm_county')
-  #check <- outcome_casecross[, c(1:16, 20, 87, 151:157)]
+  # glimpse(outcome_casecross)
+  # which(colnames(outcome_casecross)=='geo_wt_pm_zip')
+  # check <- outcome_casecross[, c(1:16, 20, 89, 111:133)]
   
   # Create a permanent case-cross over datasets
-  file_name <- paste0('../../data/health_data/', outcome_name, 
-                      '_jul_to_oct_time_strat_casecross.csv')
+  file_name <- paste0('../health_data/', j, 
+                      '_may_to_sep_time_strat_casecross.csv')
   
   # write permanent dataset
   write_csv(outcome_casecross, file_name)
   
 } # End of the overall loop
 
-# stop cluster
-stopCluster(cl)
-
-# record total time
 total_time <- Sys.time() - start
-total_time
+total_time # 7.970039 mins
 
 
 
