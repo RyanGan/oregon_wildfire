3f6c374fba969cceb112de0d633a78dcac1cd2df
AprilJing <ljy199341@163.com>
2017-04-12 11:13:38 -0600

    Update case_crossever_oregon.R

--- r_scripts/case_crossever_oregon.R
+++ r_scripts/case_crossever_oregon.R
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Title: Case crossover for diseases (Oregon State)
+# Title: Case crossover for diseases of Oregon State (Practice)
 # Author: Jingyang Liu
 # Date Created: Feb 24, 2017
 # R version: 3.3.2
