a8bba8d72e4fdfecc2c90a2fb13c520cfde19f25
AprilJing <ljy199341@163.com>
2017-05-01 12:00:04 -0600

    Update or_county_time_series.R

--- r_scripts/analysis/or_county_time_series.R
+++ r_scripts/analysis/or_county_time_series.R
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Title: Washington 2012 Time-Series Analysis
+# Title: Oregon 2013 Time-Series Analysis
 # Author: Ryan Gan
 # Date Created: 9/28/16
 # ------------------------------------------------------------------------------
@@ -10,7 +10,8 @@ library(splines) # needed to model asthma trend over time
 
 # Load county time series data
 
-path <- paste0("../../or_asthma_county_time_series.csv")
+setwd("C:/Users/jyliu/Desktop/local_git_repo/oregon_wildfire_new/r_scripts")
+path <- paste0("../data_new/county_data/or_asthma_county_time_series.csv")
 
 or_ts_df <- read_csv(path)
 # check descriptives
@@ -45,6 +46,10 @@ asthma_trend_plot <- ggplot(or_aggregate_df, aes(x = date, y = n_asthma)) +
   geom_jitter()
 plot(asthma_trend_plot)
 
+ave_geo_smk_ts <- ggplot(or_aggregate_df, aes(x = date, y = avg_geo_smk)) + 
+  geom_jitter()
+plot(ave_geo_smk_ts)
+
 # fit spline model 
 spl_yr <- bs(or_aggregate_df$date,degree=3, df=7) # spline for week/weekend trends
 
@@ -74,46 +79,39 @@ model_unadj <- glm(n_asthma ~ avg_geo_smk, or_aggregate_df, family=quasipoisson)
 summary(model_unadj)
 
 # controlling for seasonality with spline
-model_adj <- update(model_unadj,.~.+spl)
+model_adj <- update(model_unadj,.~.+spl_yr+avg_temp)
 summary(model_adj)
 # appears to be an association with asthma and wildfire smoke on the aggregate
 
-# Subset to July 1st to October 31st dates -------------------------------------
-wash_jul_oct <- or_aggregate_df %>% 
-  filter(date >= "2012-07-01" & date <= "2012-10-31" )
-# check stats
-summary(wash_jul_oct)
-
-# look at trend over Jul to oct
-ggplot(wash_jul_oct, aes(x = date, y = n_asthma)) + 
-  geom_jitter() 
+# Subset to May 1st to Sep 30th dates -------------------------------------
+# Not need to subset because using the 
 
 # fit spline to summer shape 
-spl_summer <- bs(wash_jul_oct$date,degree=3, df=3) # spline for week/weekend trends
+spl_summer <- bs(or_aggregate_df$date,degree=3, df=3) # spline for week/weekend trends
 
 # model n asthma events with a spline with 7 df (knotches)
-summer_model1 <- glm(n_asthma ~ spl_summer, wash_jul_oct, family=quasipoisson) 
+summer_model1 <- glm(n_asthma ~ spl_summer, or_aggregate_df, family=quasipoisson) 
 summary(summer_model1)
 
 # compute predicted number of asthma events from this model
 pred1 <- predict(summer_model1,type="response")
 
-plot(wash_jul_oct$date, wash_jul_oct$n_asthma,
+plot(or_aggregate_df$date, or_aggregate_df$n_asthma,
      ylim=c(0, 40),pch=19,cex=0.2,col=grey(0.6),
      main="Flexible cubic spline model",ylab="Daily Number of Asthma ER/Urgent",
      xlab="Date")
-lines(wash_jul_oct$date, pred1, lwd=2)
+lines(or_aggregate_df$date, pred1, lwd=2)
 
 # check residuals
 res1 <- residuals(summer_model1,type="response")
 
-plot(wash_jul_oct$date, res1,ylim=c(-15,15),pch=19,cex=0.4,col=grey(0.6),
+plot(or_aggregate_df$date, res1,ylim=c(-15,15),pch=19,cex=0.4,col=grey(0.6),
      main="Residuals over time",ylab="Residuals (observed-fitted)",xlab="Date")
 abline(h=1,lty=2,lwd=2)
 
 # models for association between smoke pm and daily asthma count 
 # unadjusted model
-summer_model_unadj <- glm(n_asthma ~ avg_geo_smk + avg_temp, wash_jul_oct, family=quasipoisson)
+summer_model_unadj <- glm(n_asthma ~ avg_geo_smk + avg_temp, or_aggregate_df, family=quasipoisson)
 summary(summer_model_unadj)
 
 # controlling for seasonality with spline
@@ -121,55 +119,54 @@ summer_model_adj <- update(summer_model_unadj,.~.+ spl_summer)
 summary(summer_model_adj)
 
 # going to try and limit the yearly spline to just jul 1 and oct 31
-dates_2012 <- wash_aggregate_df %>% select(date) %>% as.vector()
-jul_oct_spline <- cbind(dates_2012, spl_yr)  
+dates_2013 <- or_aggregate_df %>% select(date) %>% as.vector()
+jul_oct_spline <- cbind(dates_2013, spl_yr)  
 # subset spl_yr values 183 to 305  
-jul_oct_spline <- spl_yr[183:305, ]
+jul_oct_spline2 <- spl_yr[1:153, ]
 
-summer_model_adj_yr <- update(summer_model_unadj, .~ . + jul_oct_spline)
+summer_model_adj_yr <- update(summer_model_unadj, .~ . + jul_oct_spline2)
 summary(summer_model_adj_yr)
-# this doesn't help
 
-asthma_trend_plot <- ggplot(wash_ts_df, aes(x = date, y = asthma_n)) + 
+asthma_trend_plot <- ggplot(or_ts_df, aes(x = date, y = asthma_n)) + 
   geom_jitter() +
   facet_wrap(~ county)
 
 print(asthma_trend_plot)
 
 # try time-series in spokane county
-spokane <- wash_ts_df %>% filter(county == "Spokane" & !is.na(geo_smk_pm)) 
+douglas <- or_ts_df %>% filter(county == "Douglas" & !is.na(geo_smk_pm)) 
 
-spokane_plot <- ggplot(spokane, aes(x = date, y = asthma_n)) + 
+douglas_plot <- ggplot(douglas, aes(x = date, y = asthma_n)) + 
   geom_jitter() 
 
-print(spokane_plot)
+print(douglas_plot)
 
 # not sure a spline would help because there doesn't look to be any seasonal trends
-spl <- bs(spokane$date,degree=2,df=2) # spline for week/weekend trends
+spl <- bs(douglas$date,degree=2,df=2) # spline for week/weekend trends
 
 # model n asthma events with a spline with 7 df (knotches)
-model1 <- glm(asthma_n ~ spl, spokane, family=quasipoisson) 
+model1 <- glm(asthma_n ~ spl, douglas, family=quasipoisson) 
 summary(model1)
 
 # compute predicted number of asthma events from this model
 pred1 <- predict(model1,type="response")
 
-plot(spokane$date, spokane$asthma_n,
+plot(douglas$date, douglas$asthma_n,
      ylim=c(0, 10),pch=19,cex=0.2,col=grey(0.6),
      main="Flexible cubic spline model",ylab="Daily Number of Admissions",
      xlab="Date")
-lines(spokane$date, pred1, lwd=2)
+lines(douglas$date, pred1, lwd=2)
 
 # check residuals
 res1 <- residuals(model1,type="response")
 
-plot(spokane$date, res1,ylim=c(-15,15),pch=19,cex=0.4,col=grey(0.6),
+plot(douglas$date, res1,ylim=c(-15,15),pch=19,cex=0.4,col=grey(0.6),
      main="Residuals over time",ylab="Residuals (observed-fitted)",xlab="Date")
 abline(h=1,lty=2,lwd=2)
 
 # models for association between smoke pm and daily asthma count 
 # unadjusted model
-model_unadj <- glm(asthma_n ~ geo_smk_pm, spokane, family=quasipoisson)
+model_unadj <- glm(asthma_n ~ geo_smk_pm, douglas, family=quasipoisson)
 summary(model_unadj)
 
 # controlling for seasonality with spline
