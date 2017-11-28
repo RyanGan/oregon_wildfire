557662d285ee883dd60eb42adab88a187a5c3836
AprilJing <ljy199341@163.com>
2017-11-28 15:49:52 -0700

    delete cleaning code and extra about making saba data set; write saba index; make asthma at risk data set

--- r_scripts/data_wrangle/asthma_cohort.R
+++ r_scripts/data_wrangle/asthma_cohort.R
@@ -1,3 +1,4 @@
+gitpull
 # ------------------------------------------------------------------------------
 # Title: Basic data wrangling  for 2013 Oregon data and seperating by ICD9 lists
 # Author: Jingyang Liu
@@ -7,8 +8,7 @@
 # ------------------------------------------------------------------------------
 
 # This file is for filtering saba outcomes.
-# Also it separates the data into 12 disease files due to the ICD9 lists.
-# And combine a asthma at-risk data set with ICD9 of asthma or saba.
+# And combine an asthma at-risk data set with ICD9 of asthma or saba.
 # original data sets and instrunction files such as index are stored in data_original file,
 # the new data sets we made are stored in data_new, which contain health and smoke
 # data sets. Now the outcomes are all stored in data_new/health.
@@ -32,29 +32,13 @@ stop_time <-  Sys.time() - start_time
 # time it took
 stop_time # 7 mins
 
-
-# basic cleaning ---------------------------------------------------------------
-oregon_df <- oregon_df %>% 
-  # filter State is Oregon
-  filter(STATE=="OR") 
-
 write_path <- paste0('../../../data/data_original/',
                      '2013_oregon_epis_care.csv') # csv file, for future use
 write_csv(oregon_df, write_path)
 ### ----------------------------------------------------------------------------
 
 
-### Make data set
-
-# path and file name of data
-read_path <- paste0("../../../data/data_original/2013_oregon_epis_care.csv")
-start_time <- Sys.time()
-oregon_df <- fread(read_path, sep = ",", 
-                   colClasses = rep("character", 72)) 
-stop_time <-  Sys.time() - start_time 
-# time it took
-stop_time # 14 min
-
+### Make saba index
 
 # the ndc beta-2 agonist file are made in SQLite previously.
 read_path2 <- paste0("../../../data/data_original/2013_ndc_beta2_agonists.csv")
@@ -74,11 +58,23 @@ oregon_saba_index <- ndc_in_oregon_df %>%
   select(personkey) %>%
   unique() # unique personkey, 109483
 
-saba_df <- oregon_df %>%
-  filter(personkey %in% oregon_saba_index$personkey) # 11358660, ndc only data set
+write_path <- paste0('../../../data/data_new/health/2013_oregon_saba_index.csv')
+write_csv(oregon_saba_index, write_csv)
+### ----------------------------------------------------------------------------
 
 
-### Import ICD9 code------------------------------------------------------------
+
+### Make data set of saba or asthma
+# path and file name of data
+read_path <- paste0("../../../data/data_original/2013_oregon_epis_care.csv")
+start_time <- Sys.time()
+oregon_df <- fread(read_path, sep = ",", 
+                   colClasses = rep("character", 72)) 
+stop_time <-  Sys.time() - start_time 
+# time it took
+stop_time # 14 min
+
+## Import ICD9 code-------------------------------------------------------------
 icd9_key <- read_excel("../../../data/data_original/CMS32_DESC_LONG_SHORT_DX.xlsx") %>% 
   # rename the terrible variable names
   select(dx_code = 1, long_desc = 2, short_desc = 3)
@@ -91,18 +87,16 @@ icd9_key <- arrange(icd9_key, dx_code) %>%
 which(icd9_key$dx_code == '49300') # start of asthma is row 5206
 which(icd9_key$dx_code == '49392') # end of asthma is row 5219
 
-# limit just to asthma code and just the diagnosis column
-icd9_check <- icd9_key %>%
-  filter(n >= 5206 & n <= 5219) 
-icd9_check
-
 asthma_icd9 <- filter(icd9_key, n >= 5206 & n <= 5219) %>%
   select(dx_code)
 # convert to vector
 asthma_icd9 <- as.vector(as.matrix(asthma_icd9))   
 
+## -----------------------------------------------------------------------------
+
 ## data set with saba or icd
 saba_asthma_df <- oregon_df %>%
+  filter(STATE == "OR")
   filter(personkey %in% oregon_saba_index$personkey |
            dx1 %in% asthma_icd9 |
            dx2 %in% asthma_icd9 |
