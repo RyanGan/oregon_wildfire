119f33b50fbbfd0840a40276532b3a61d216cad7
AprilJing <ljy199341@163.com>
2017-11-30 13:02:20 -0700

    delete RData making part

--- r_scripts/data_wrangle/icd9_ndc_outcome.R
+++ r_scripts/data_wrangle/icd9_ndc_outcome.R
@@ -52,64 +52,6 @@ write_csv(saba_df, write_path3)
 
 # ------------------------------------------------------------------------------
 
-# read in icd9 data and create lists of vectors of icd9 codes ----
-# read in xlsx icd9 codes and description
-icd9_df <- read_excel("../../../data/data_original/CMS32_DESC_LONG_SHORT_DX.xlsx") %>% 
-  # rename the terrible variable names
-  select(dx_code = 1, long_desc = 2, short_desc = 3)
-
-# look at the first 6 rows of the icd9 codes; comes in sorted
-head(icd9_df)
-glimpse(icd9_df)
-
-# Overall goal is to make a list of vectors for each outcome that contains the
-# character strings of icd9 codes that make up the outcome of interest.
-# Some outcomes like COPD, where icd9 codes are not sequential, will not work
-# and will need to be entered by hand.
-
-# Create a dataframe of the outcome name, start, and stop rows
-outcome_name <- c("respiratory", "asthma", "pneumonia", "acute_bronch",
-                  "cvd", "arrhythmia", "cerbrovas_dis", "heart_failure",
-                  "isch_heart_dis", "myocardial_infarc", "broken_arm")
-# first icd9 code of outcome
-start_icd9 <- c("460", "49300", "4800", "4660", "390", "4270", "430", "4280",
-                "41000", "41000", "81300")
-
-# last icd9 code of outcome
-stop_icd9 <- c("5199", "49392", "486", "46619", "4599", "4279", "4389", "4289",
-               "4139", "41092", "81393")
-
-# dataframe of outcomes and start stop of icd9 codes
-outcome_icd9_range <- data_frame(outcome_name, start_icd9, stop_icd9)
-
-# list of icd9 codes -----------------------------------------------------------
-outcome_icd9_list <- apply(outcome_icd9_range, 1, 
-                           function(x) { # start of function
-                             icd9_df %>% 
-                               slice(which(icd9_df$dx_code == x[2]): which(icd9_df$dx_code == x[3])) %>%
-                               select(1) %>%
-                               as_vector()
-                           } )
-
-# assign list names
-names(outcome_icd9_list) <- outcome_name
-
-# Add COPD ICD9 codes to the list
-# COPD, ICD9 490 to 492, 494, and 496 
-copd_icd9 <- c('490', '4910','4911','49120','49121','49122','4918','4919', '4920',
-               '4928', '4940', '4941', '496') 
-
-# append copd list to icd9 list
-outcome_icd9_list <- c(outcome_icd9_list, list(copd = copd_icd9))
-
-# I don't know if not having atomic/flat values for each element of the list
-# will matter, but I need to try it out before I'll know
-
-# save R file to use in other scripts
-save_path <- paste0("../../../data/data_original/outcome_list.RData")
-save(outcome_icd9_list, file = save_path)
-
-
 
 # Define read paths ------------ -----------------------------------------------
 # import r list of outcome-specific icd9 codes
