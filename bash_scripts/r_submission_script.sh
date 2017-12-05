71b53a676822c1169efbe7542e4e75b85a4a6c66
Ryan Gan <ryangan@ozone.atmos.colostate.edu>
2017-12-05 07:15:43 -0700

    added unlimited memmory command to bash script to prevent system kill

--- bash_scripts/r_submission_script.sh
+++ bash_scripts/r_submission_script.sh
@@ -3,11 +3,14 @@
 #PBS -l nodes=1:ppn=1
 #PBS -W group_list=pierce_group
 #PBS -q batch
+#PBS -M rgan@colostate.edu
+#PBS -m abe
+
+# set memory to unlimited
+ulimit -s unlimited
 
 # set command directory
 cd /home/ryangan/local_git_repo/oregon_wildfire/
-# print directory
-pwd
 
 # export R library
 export R_LIB=~/R/x86_64-pc-linux-gnu-library/3.3
