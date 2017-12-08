43ab1f3844530c25fa25358195f1fcc49ed97c5c
RyanGan <ryan.william.gan@gmail.com>
2017-12-08 16:35:39 -0500

    updated bash script to run asthma_cohort_fireseason.

--- bash_scripts/r_submission_script.sh
+++ bash_scripts/r_submission_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N ryan_rjob
-#PBS -l nodes=1:ppn=16
+#PBS -l nodes=1:ppn=1
 #PBS -W group_list=pierce_group
 #PBS -q batch
 #PBS -M rgan@colostate.edu
@@ -16,5 +16,5 @@ cd /home/ryangan/local_git_repo/oregon_wildfire/
 export R_LIB=~/R/x86_64-pc-linux-gnu-library/3.3
 
 # run rscript
-Rscript --vanilla ./r_scripts/data_wrangle/asthma_cohort.R
+Rscript --vanilla ./r_scripts/data_wrangle/asthma_cohort_fireseason.R
 
