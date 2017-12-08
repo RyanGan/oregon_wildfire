5381578f121ede050e12ac5f7339904957cb6792
RyanGan <ryan.william.gan@gmail.com>
2017-12-08 12:20:44 -0500

    added server execution script.

---
+++ r_scripts/analysis/markdown_server_execute.R
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+# Title: Script to execute markdown files on a server
+# Author: Ryan Gan
+# Date Created: 2017-12-08
+# ------------------------------------------------------------------------------
+
+# call R markdown file
+rmarkdown::render('./r_scripts/analysis/asthma_cohort_descriptive_epi.Rmd')
+
