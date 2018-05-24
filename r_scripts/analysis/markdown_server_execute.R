9f3e1d51876d4e193d501b01c4dccb84eb7a6866
RyanGan <ryan.william.gan@gmail.com>
2018-05-24 15:03:56 -0400

    Moved scripts out of repo with analyses that didn't pan out.

--- r_scripts/analysis/markdown_server_execute.R
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------------------
-# Title: Script to execute markdown files on a server
-# Author: Ryan Gan
-# Date Created: 2017-12-08
-# ------------------------------------------------------------------------------
-
-# call R markdown file
-rmarkdown::render('./r_scripts/analysis/asthma_cohort_descriptive_epi.Rmd')
-
